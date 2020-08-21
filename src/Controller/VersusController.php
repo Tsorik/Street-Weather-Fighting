2a473235c4e02111f0f3109e5135aa2dfb434b22
Tsorik <kamykaz13010@gmail.com>
2020-08-21 11:59:31 +0200

    deux ville request

--- src/Controller/VersusController.php
+++ src/Controller/VersusController.php
@@ -16,28 +16,42 @@ class VersusController extends AbstractController
         $id1 = $request->get('id1');
         $id2 = $request->get('id2');
 
-        $insee = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $id1 . '');
+        $insee1 = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $id1 . '');
+        $insee2 = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $id2 . '');
 
-        if ($insee !== false) {
-            $table_cities = json_decode($insee);
+        if ($insee1 !== false) {
+            $table_cities = json_decode($insee1);
             $cities = $table_cities->{'cities'};
             $cities = $cities[0];
-            $insee_num = $cities->{'insee'};
+            $insee_num1 = $cities->{'insee'};
+        }
+        if ($insee2 !== false) {
+            $table_cities = json_decode($insee2);
+            $cities = $table_cities->{'cities'};
+            $cities = $cities[0];
+            $insee_num2 = $cities->{'insee'};
         }
 
-        $data = file_get_contents('https://api.meteo-concept.com/api/forecast/daily/0?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&insee=' . $insee_num . '');
+        $data1 = file_get_contents('https://api.meteo-concept.com/api/forecast/daily/0?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&insee=' . $insee_num1 . '');
+        $data2 = file_get_contents('https://api.meteo-concept.com/api/forecast/daily/0?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&insee=' . $insee_num2 . '');
 
-
-        if ($data !== false) {
-            $decoded = json_decode($data);
+        if ($data1 !== false) {
+            $decoded = json_decode($data1);
             $city = $decoded->city;
             $forecast = $decoded->forecast;
 
-            $info = "Aujourd'hui à {$city->name}, on prévoit {$forecast->rr10}mm (pas plus de {$forecast->rr1}mm en tous cas) de précipitations.";
+            $info1 = "Aujourd'hui à {$city->name}, on prévoit {$forecast->rr10}mm (pas plus de {$forecast->rr1}mm en tous cas) de précipitations.";
         }
+        if ($data2 !== false) {
+            $decoded = json_decode($data2);
+            $city = $decoded->city;
+            $forecast = $decoded->forecast;
 
+            $info2 = "Aujourd'hui à {$city->name}, on prévoit {$forecast->rr10}mm (pas plus de {$forecast->rr1}mm en tous cas) de précipitations.";
+        }
         return $this->render('versus/index.html.twig', [
-            'info' => $info,
+            'info1' => $info1,
+            'info2' => $info2,
         ]);
     }
 }
