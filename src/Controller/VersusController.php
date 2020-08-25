b98a4c9f3c32b7cd033f0bd3190382d3d9ee4665
Tsorik <kamykaz13010@gmail.com>
2020-08-25 17:19:37 +0200

    tableau versus

--- src/Controller/VersusController.php
+++ src/Controller/VersusController.php
@@ -13,45 +13,53 @@ class VersusController extends AbstractController
      */
     public function index(Request $request)
     {
-        $id1 = $request->get('id1');
-        $id2 = $request->get('id2');
+        $city_name1 = $request->get('id1');
+        $city_name2 = $request->get('id2');
 
-        $insee1 = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $id1 . '');
-        $insee2 = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $id2 . '');
+        /* weather api request insee code */
+        $insee1 = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $city_name1 . '');
+        $insee2 = file_get_contents('https://api.meteo-concept.com/api/location/cities?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&search='. $city_name2 . '');
 
-        if ($insee1 !== false) {
-            $table_cities = json_decode($insee1);
-            $cities = $table_cities->{'cities'};
-            $cities = $cities[0];
-            $insee_num1 = $cities->{'insee'};
+        if ($insee1 !== false && $insee2 !== false) {
+            $table_cities1 = json_decode($insee1);
+            $cities1 = $table_cities1->{'cities'};
+            $cities1 = $cities1[0];
+            $insee_num1 = $cities1->{'insee'};
+
+            $table_cities2 = json_decode($insee2);
+            $cities2 = $table_cities2->{'cities'};
+            $cities2 = $cities2[0];
+            $insee_num2 = $cities2->{'insee'};
         }
-        if ($insee2 !== false) {
-            $table_cities = json_decode($insee2);
-            $cities = $table_cities->{'cities'};
-            $cities = $cities[0];
-            $insee_num2 = $cities->{'insee'};
-        }
 
+        /* weather api request data */
         $data1 = file_get_contents('https://api.meteo-concept.com/api/forecast/daily/0?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&insee=' . $insee_num1 . '');
         $data2 = file_get_contents('https://api.meteo-concept.com/api/forecast/daily/0?token=69465bc653ad80ed00bfa483dfc17a1b5f50bc9354c117c238f7d093454754fd&insee=' . $insee_num2 . '');
 
-        if ($data1 !== false) {
-            $decoded = json_decode($data1);
-            $city = $decoded->city;
-            $forecast = $decoded->forecast;
+        if ($data1 !== false && $data2 !== false) {
+            $decoded1 = json_decode($data1);
+            $city1 = $decoded1->city;
+            $forecast1 = $decoded1->forecast;
 
-            $info1 = "Aujourd'hui à {$city->name}, on prévoit {$forecast->rr10}mm (pas plus de {$forecast->rr1}mm en tous cas) de précipitations.";
-        }
-        if ($data2 !== false) {
-            $decoded = json_decode($data2);
-            $city = $decoded->city;
-            $forecast = $decoded->forecast;
+            $decoded2 = json_decode($data2);
+            $city2 = $decoded2->city;
+            $forecast2 = $decoded2->forecast;
 
-            $info2 = "Aujourd'hui à {$city->name}, on prévoit {$forecast->rr10}mm (pas plus de {$forecast->rr1}mm en tous cas) de précipitations.";
+            dump($forecast1);
+            $tmax = $forecast1->tmax;
+            $tmin = $forecast1->tmin;
         }
+        
+        /* "Aujourd'hui à {$city1->name}, on prévoit {$forecast1->rr10}mm (pas plus de {$forecast1->rr1}mm en tous cas) de précipitations."; */
+        
         return $this->render('versus/index.html.twig', [
-            'info1' => $info1,
-            'info2' => $info2,
+            'city_name1' => $city_name1,
+            'city_name2' => $city_name2,
+            'tmax1' => $forecast1->tmax,
+            'tmax2' => $forecast2->tmax,
+            'tmin1' => $forecast1->tmin,
+            'tmin2' => $forecast2->tmin,
+            'info' => $forecast1,
         ]);
     }
 }
